dfa043d979c5794e6f26b6bc622d658bed398e5a
BrunoCarvalhs <bruno.silva.carvalho@outlook.com.br>
2021-06-22 21:07:08 -0300

    dep (payflow) dependencies of firebase and setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/app_widget.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(App());
 }
 
-class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                "Não foi possível inicializar o Firebase",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done)
+          return AppWidget();
+        return SplashPage();
+      },
     );
   }
 }
