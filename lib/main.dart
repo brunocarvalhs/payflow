255db1541d884cca5e9ab8a0299a8ed5f68336b4
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2022-01-23 12:03:20 -0300

    base apllication

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,42 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/app_widget.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
+import 'app_widget.dart';
 
 void main() {
-  runApp(App());
+  runApp(AppFirebase());
 }
 
-class App extends StatefulWidget {
+class AppFirebase extends StatefulWidget {
   @override
-  _AppState createState() => _AppState();
+  _AppFirebaseState createState() => _AppFirebaseState();
 }
 
-class _AppState extends State<App> {
+class _AppFirebaseState extends State<AppFirebase> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Material(
-            child: Center(
-              child: Text(
-                "Não foi possível inicializar o Firebase",
-                textDirection: TextDirection.ltr,
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Material(
+              child: Center(
+                child: Text(
+                  "Não foi possível inicializar o Firebase",
+                  textDirection: TextDirection.ltr,
+                ),
               ),
-            ),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.done)
-          return AppWidget();
-        return SplashPage();
-      },
-    );
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUser(context);
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
-          Center(
-            child: Image.asset(AppImages.union),
-          ),
-          Center(
-            child: Image.asset(AppImages.logoFull),
-          ),
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull)),
         ],
       ),
     );
